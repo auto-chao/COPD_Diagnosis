2398a7cf347ecbb347474560b46eac99c93f56c0
kookers <auto_chao@mail.ustc.edu.cn>
2024-04-09 15:28:11 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ cd ..
 
 python ./main_ATE.py --learning_rate 1e-6 --ATE_num 1  --dropout 0.35 --epochs 100  --batch_size 32
 python ./main_LINCLS.py --learning_rate 1e-6 --ATE_num 1 --epochs 100  --batch_size 32	
-python ./main_LINCLS.py --mode test --learning_rate 1e-6 --ATE_num 1 --epochs 100  --batch_size 32	
+python ./main_LINCLS.py --mode test --cls_learning_rate 1e-6 --ATE_num 1 --epochs 100  --batch_size 32	
